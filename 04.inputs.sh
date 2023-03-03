ee7614fd0ad8e2f6043f97d8605a34a312d07ffe
navaneetha <navani91@gmail.com>
2023-03-03 17:12:25 +0000

    inputs

--- 04.inputs.sh
+++ 04.inputs.sh
@@ -1,3 +1,10 @@
-echo $1
-echo $2
+# 0-n, $*/$@, $#
+echo First Argument $1 # First Argument
+echo Second Argument $2 # Second Argument
 
+echo script Name $0 # script Name
+
+echo All Arguments $*
+echo All Arguments $@ # All Arguments
+
+echo Numberof Arguments $# # Numberof Arguments
