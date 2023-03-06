b763327cb154091888d00d2450f9ea7859b92f13
navaneetha <navani91@gmail.com>
2023-03-06 17:56:30 +0000

    conditions

---
+++ 06.conditions.sh
@@ -0,0 +1,9 @@
+number-$1
+string=$2
+
+if [ $(number) -eq 5 ]; then # then can go in next line as well, also semi colon is also file in the same line
+f1
+
+if [ $(string) -- abc ]; then
+  echo string is abc
+f1
